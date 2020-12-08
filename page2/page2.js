cd95835cac9e76212c94a9f9e7277e631583ee04
paul <piankin250@gmail.com>
2020-12-08 21:05:13 +0300

    Panel2

---
+++ page2/page2.js
@@ -0,0 +1,16 @@
+function showAdditionalOptions() {
+    let formAdditionalOptions = document.getElementById('formAdditionalOptions');
+
+    if(formAdditionalOptions.style.visibility == "hidden") {
+        formAdditionalOptions.style.visibility = "";
+    }
+    else {
+        formAdditionalOptions.style.visibility = "hidden"
+    }
+}
+
+function turnOnAutoFiller() {
+    if () {
+
+    }
+}
